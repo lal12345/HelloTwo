e2e4ed5401b48a6479a664134b83b33e286d60fe
k.kalaiselvam <k.kalaiselvam@BDC3-D-519QSZK.dir.svc.accenture.com>
2016-12-22 16:46:14 +0530

    asa

--- src/test/java/org/cloudfoundry/samples/music/web/controllers/TestEmployeeDetails.java
+++ src/test/java/org/cloudfoundry/samples/music/web/controllers/TestEmployeeDetails.java
@@ -12,6 +12,7 @@ public class TestEmployeeDetails {
    @Test
    public void testCalculateAppriasal() {
 	   System.out.println("new_gradle_change");
+	   System.out.println("new_gradle_change1");
       employee.setName("Rajeev");
       employee.setAge(25);
       employee.setMonthlySalary(8000);
