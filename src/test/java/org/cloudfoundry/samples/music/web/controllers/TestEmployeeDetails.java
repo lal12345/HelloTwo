d158fb94e1aecd2fe6d52b5aceabeaf0bdf16ebd
laldhari.prasad <laldhari.prasad@BDC3-D-519QT07.dir.svc.accenture.com>
2016-12-23 12:21:06 +0530

    changes10

--- src/test/java/org/cloudfoundry/samples/music/web/controllers/TestEmployeeDetails.java
+++ src/test/java/org/cloudfoundry/samples/music/web/controllers/TestEmployeeDetails.java
@@ -34,6 +34,7 @@ public class TestEmployeeDetails {
       System.out.println("new_gradle_change7");
       System.out.println("new_gradle_change8");
       System.out.println("new_gradle_change9");
+      System.out.println("new_gradle_change10");
    }
 
    // test to check yearly salary
